dfe0a05e14629d4b9a04e8bfe80e6743ac7055d5
Cownic <Lewkh1999@gmail.com>
2023-10-13 12:34:43 +0800

    ver 1

--- helper/utility.py
+++ helper/utility.py
@@ -7,6 +7,19 @@ def load_data(file_path):
     return pd.read_csv(file_path)
 
 
+def merge_data(df1, df2, on_v):
+    return pd.merge(df1, df2, how='left' , on=on_v)
+
+def drop_dups(df):
+    # We are keeping the last entry for those repeated ones
+    return df.drop_duplicates(subset=['parcelid' , 'transactiondate'], ignore_index=True, keep='last')
+
+def check_duplicates(housing): 
+    idsUnique = len(housing[['parcelid', 'transactiondate']].value_counts())
+    idsTotal = housing.shape[0]
+    idsDupli = idsTotal - idsUnique
+    print("There are " + str(idsDupli) + " duplicate IDs for " + str(idsTotal) + " total entries")
+
 def print_percent_missing(df):
     missing_percent = []
     total_count = len(df)
@@ -19,3 +32,4 @@ def print_percent_missing(df):
 
     for feature, m_percent in missing_percent:
         print(f'{feature} : {m_percent}%\n')
+
